02edd7b7aaaf53152bc2310b88774f0071dbfa52
ExplodingUFO <1034917216@qq.com>
2026-04-14 00:35:24 +0800

    test: cover surface scene render path

--- tests/Videra.SurfaceCharts.Avalonia.IntegrationTests/SurfaceScenePainterTests.cs
+++ tests/Videra.SurfaceCharts.Avalonia.IntegrationTests/SurfaceScenePainterTests.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Avalonia;
+using Avalonia.Media;
 using FluentAssertions;
 using Videra.SurfaceCharts.Avalonia.Controls;
 using Videra.SurfaceCharts.Core;
@@ -12,43 +13,131 @@ public sealed class SurfaceScenePainterTests
     [Fact]
     public void ProjectTriangles_WithBuiltScene_ReturnsProjectedTriangles()
     {
-        var metadata = CreateMetadata(width: 2, height: 2);
-        var tile = SurfaceChartTestHelpers.CreateTile(metadata, new SurfaceTileKey(0, 0, 0, 0), tileValue: 12f);
-        var scene = new SurfaceRenderer().BuildScene(metadata, [tile], CreateColorMap(metadata));
+        AvaloniaHeadlessTestSession.Run(() =>
+        {
+            var metadata = CreateMetadata(width: 2, height: 2);
+            var tile = SurfaceChartTestHelpers.CreateTile(metadata, new SurfaceTileKey(0, 0, 0, 0), tileValue: 12f);
+            var scene = new SurfaceRenderer().BuildScene(metadata, [tile], CreateColorMap(metadata));
 
-        var triangles = SurfaceScenePainter.ProjectTriangles(scene, new Size(320, 180));
+            var triangles = SurfaceScenePainter.ProjectTriangles(scene, new Size(320, 180));
 
-        triangles.Should().HaveCount(2);
-        triangles.Should().OnlyContain(static triangle => HasFinitePoints(triangle) && triangle.A != triangle.B);
+            triangles.Should().HaveCount(2);
+            triangles.Should().OnlyContain(static triangle => HasFinitePoints(triangle) && triangle.A != triangle.B);
+        });
     }
 
     [Fact]
     public void ProjectTriangles_WithoutSceneOrTiles_ReturnsEmpty()
     {
-        var metadata = CreateMetadata(width: 2, height: 2);
-        var emptyScene = new SurfaceRenderScene(metadata, []);
+        AvaloniaHeadlessTestSession.Run(() =>
+        {
+            var metadata = CreateMetadata(width: 2, height: 2);
+            var emptyScene = new SurfaceRenderScene(metadata, []);
 
-        SurfaceScenePainter.ProjectTriangles(null, new Size(320, 180)).Should().BeEmpty();
-        SurfaceScenePainter.ProjectTriangles(emptyScene, new Size(320, 180)).Should().BeEmpty();
+            SurfaceScenePainter.ProjectTriangles(null, new Size(320, 180)).Should().BeEmpty();
+            SurfaceScenePainter.ProjectTriangles(emptyScene, new Size(320, 180)).Should().BeEmpty();
+        });
     }
 
     [Fact]
-    public async Task SurfaceChartView_LoadedTiles_BuildsRenderSceneConsumableByPainter()
+    public void DrawScene_WithBuiltScene_IssuesGeometryDrawCalls()
     {
-        var metadata = CreateMetadata(width: 4, height: 4);
-        var source = new ScriptedSurfaceTileSource(metadata, defaultTileValue: 6f);
-        var view = new SurfaceChartView();
+        AvaloniaHeadlessTestSession.Run(() =>
+        {
+            var metadata = CreateMetadata(width: 2, height: 2);
+            var tile = SurfaceChartTestHelpers.CreateTile(metadata, new SurfaceTileKey(0, 0, 0, 0), tileValue: 12f);
+            var scene = new SurfaceRenderer().BuildScene(metadata, [tile], CreateColorMap(metadata));
+            var expectedTriangleCount = SurfaceScenePainter.ProjectTriangles(scene, new Size(320, 180)).Count;
+            using var drawingContext = new RecordingDrawingContext();
 
-        view.Measure(new Size(240, 160));
-        view.Arrange(new Rect(0, 0, 240, 160));
-        view.Source = source;
+            SurfaceScenePainter.DrawScene(drawingContext.Context, scene, new Size(320, 180));
 
-        await SurfaceChartTestHelpers.WaitForLoadedTileValuesAsync(view, [6f]);
+            expectedTriangleCount.Should().BeGreaterThan(0);
+            drawingContext.GeometryDrawCallCount.Should().Be(expectedTriangleCount);
+        });
+    }
 
-        var scene = GetRenderScene(view);
+    [Fact]
+    public void DrawScene_WithoutSceneOrTiles_IssuesNoGeometryDrawCalls()
+    {
+        AvaloniaHeadlessTestSession.Run(() =>
+        {
+            var metadata = CreateMetadata(width: 2, height: 2);
+            var emptyScene = new SurfaceRenderScene(metadata, []);
+            using var drawingContext = new RecordingDrawingContext();
 
-        scene.Should().NotBeNull();
-        SurfaceScenePainter.ProjectTriangles(scene, new Size(240, 160)).Should().HaveCount(18);
+            SurfaceScenePainter.DrawScene(drawingContext.Context, null, new Size(320, 180));
+            SurfaceScenePainter.DrawScene(drawingContext.Context, emptyScene, new Size(320, 180));
+
+            drawingContext.GeometryDrawCallCount.Should().Be(0);
+        });
+    }
+
+    [Fact]
+    public Task SurfaceChartView_LoadedTiles_BuildsRenderSceneConsumableByPainter()
+    {
+        return AvaloniaHeadlessTestSession.RunAsync(async () =>
+        {
+            var metadata = CreateMetadata(width: 4, height: 4);
+            var source = new ScriptedSurfaceTileSource(metadata, defaultTileValue: 6f);
+            var view = new SurfaceChartView();
+
+            view.Measure(new Size(240, 160));
+            view.Arrange(new Rect(0, 0, 240, 160));
+            view.Source = source;
+
+            await SurfaceChartTestHelpers.WaitForLoadedTileValuesAsync(view, [6f]);
+
+            var scene = GetRenderScene(view);
+
+            scene.Should().NotBeNull();
+            SurfaceScenePainter.ProjectTriangles(scene, new Size(240, 160)).Should().HaveCount(18);
+        });
+    }
+
+    [Fact]
+    public Task SurfaceChartView_Render_WithLoadedTiles_IssuesGeometryDrawCalls()
+    {
+        return AvaloniaHeadlessTestSession.RunAsync(async () =>
+        {
+            var metadata = CreateMetadata(width: 4, height: 4);
+            var source = new ScriptedSurfaceTileSource(metadata, defaultTileValue: 6f);
+            var view = new SurfaceChartView();
+
+            view.Measure(new Size(240, 160));
+            view.Arrange(new Rect(0, 0, 240, 160));
+            view.Source = source;
+
+            await SurfaceChartTestHelpers.WaitForLoadedTileValuesAsync(view, [6f]);
+
+            var scene = GetRenderScene(view);
+            scene.Should().NotBeNull();
+
+            var expectedTriangleCount = SurfaceScenePainter.ProjectTriangles(scene, view.Bounds.Size).Count;
+            expectedTriangleCount.Should().BeGreaterThan(0);
+            using var drawingContext = new RecordingDrawingContext();
+
+            view.Render(drawingContext.Context);
+
+            drawingContext.GeometryDrawCallCount.Should().Be(expectedTriangleCount);
+        });
+    }
+
+    [Fact]
+    public void SurfaceChartView_Render_WithoutRenderScene_IssuesNoGeometryDrawCalls()
+    {
+        AvaloniaHeadlessTestSession.Run(() =>
+        {
+            var view = new SurfaceChartView();
+            using var drawingContext = new RecordingDrawingContext();
+
+            view.Measure(new Size(240, 160));
+            view.Arrange(new Rect(0, 0, 240, 160));
+
+            view.Render(drawingContext.Context);
+
+            drawingContext.GeometryDrawCallCount.Should().Be(0);
+        });
     }
 
     private static SurfaceMetadata CreateMetadata(int width, int height)
@@ -81,5 +170,54 @@ public sealed class SurfaceScenePainterTests
             && double.IsFinite(triangle.B.Y)
             && double.IsFinite(triangle.C.X)
             && double.IsFinite(triangle.C.Y);
+    }
+
+    private sealed class RecordingDrawingContext : IDisposable
+    {
+        private readonly DrawingGroup _drawingGroup = new();
+        private readonly DrawingContext _drawingContext;
+        private int? _geometryDrawCallCount;
+
+        public RecordingDrawingContext()
+        {
+            _drawingContext = _drawingGroup.Open();
+        }
+
+        public DrawingContext Context => _drawingContext;
+
+        public int GeometryDrawCallCount
+        {
+            get
+            {
+                CompleteRecording();
+                return _geometryDrawCallCount!.Value;
+            }
+        }
+
+        public void Dispose()
+        {
+            CompleteRecording();
+        }
+
+        private void CompleteRecording()
+        {
+            if (_geometryDrawCallCount.HasValue)
+            {
+                return;
+            }
+
+            _drawingContext.Dispose();
+            _geometryDrawCallCount = CountGeometryDrawings(_drawingGroup);
+        }
+
+        private static int CountGeometryDrawings(Drawing drawing)
+        {
+            return drawing switch
+            {
+                GeometryDrawing => 1,
+                DrawingGroup group => group.Children.Sum(CountGeometryDrawings),
+                _ => 0,
+            };
+        }
     }
 }
